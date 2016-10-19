3bae8346b1cf0af42904e6d2e39ec23f7c97740c
Braden Jageman <bjageman@gmail.com>
2016-10-19 08:32:45 +0000

    Fixed the loop bugs

--- gpiorecord.py
+++ gpiorecord.py
@@ -7,21 +7,21 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(22, GPIO.OUT)
 GPIO.setup(12, GPIO.IN)
 
-#subprocess.Popen(["rec","pytest.wav","silence","1","5","2%"])
-#time.sleep(10)
-#subprocess.Popen(["pkill","rec"])
-
 recording = False
 
 try:
     while True:
+        sleep(1)
         if (GPIO.input(12)):
-            print("BUTTON IS ON")
-            subprocess.Popen(["rec","pytest.wav","silence","1","5","2%"])
-            recording = True
+            if (recording == False):
+                recording = True
+                print("BUTTON IS ON")
+                subprocess.Popen(["rec","pytest.wav","silence","1","5","2%"])
         else:
+            print("BUTTON IS OFF")
             if (recording):
                 subprocess.Popen(["pkill","rec"])
                 recording = False
+                subprocess.Popen(["aplay","pytest.wav"])
 except KeyboardInterrupt:
-    pass
+    GPIO.cleanup()
